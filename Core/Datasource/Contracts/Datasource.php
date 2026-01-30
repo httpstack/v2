4de437a52c870d825b0ac4e577d8c1695e847965
httpstack <httpstack@cmcintosh.net>
2026-01-30 02:43:30 -0800

    Update Datasource.php

--- Core/Datasource/Contracts/Datasource.php
+++ Core/Datasource/Contracts/Datasource.php
@@ -4,21 +4,8 @@ namespace Core\Datasource\Contracts;
 
 interface Datasource
 {
-    // This class assumes you have a instance property called readOnly
-    // this property should default to true.
-
-    /**
-     * Set the read-only status of the datasource.
-     *
-     * @param bool $readOnly
-     * @return void
-     */
+    public function push(array $attributes): void;
+    public function pull(): void;
     public function setReadOnly(bool $readOnly): void;
-
-    /**
-     * Check if the datasource is read-only.
-     *
-     * @return bool
-     */
     public function isReadOnly(): bool;
 }
